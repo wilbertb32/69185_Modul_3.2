df5e798b4a726438e8807b37594df47dceb1a386
wilbertb32 <wilbertbernardi2004@gmail.com>
2025-02-22 15:36:08 +0700

    Modul 3.2 Step 9

--- app/src/main/java/com/example/compose/rally/RallyActivity.kt
+++ app/src/main/java/com/example/compose/rally/RallyActivity.kt
@@ -101,44 +101,54 @@ fun RallyApp() {
                 )
             }
         ) { innerPadding ->
-            NavHost(
+            RallyNavHost(
                 navController = navController,
-                startDestination = RallyActivity.Overview.route, // Pastikan Overview menjadi startDestination
                 modifier = Modifier.padding(innerPadding)
-            ) {
-                // Tambahkan rute Overview di sini
-                composable(route = RallyActivity.Overview.route) {
-                    OverviewScreen(
-                        onAccountClick = { accountType ->
-                            navController.navigateSingleTopTo("${SingleAccount.route}/$accountType")
-                        }
-                    )
-                }
-                composable(route = Accounts.route) {
-                    AccountsScreen(
-                        onAccountClick = { accountType ->
-                            navController.navigateSingleTopTo("${SingleAccount.route}/$accountType")
-                        }
-                    )
-                }
-                composable(route = Bills.route) {
-                    BillsScreen()
-                }
-                composable(
-                    route = SingleAccount.routeWithArgs,
-                    arguments = SingleAccount.arguments,
-                    deepLinks = SingleAccount.deepLinks
-                ) { navBackStackEntry ->
-                    val accountType =
-                        navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
-
-                    SingleAccountScreen(accountType)
-                }
-            }
+            )
         }
     }
 }
 
+@Composable
+fun RallyNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = RallyActivity.Overview.route, // Pastikan Overview menjadi startDestination
+        modifier = Modifier
+    ) {
+        // Tambahkan rute Overview di sini
+        composable(route = RallyActivity.Overview.route) {
+            OverviewScreen(
+                onAccountClick = { accountType ->
+                    navController.navigateSingleTopTo("${SingleAccount.route}/$accountType")
+                }
+            )
+        }
+        composable(route = Accounts.route) {
+            AccountsScreen(
+                onAccountClick = { accountType ->
+                    navController.navigateSingleTopTo("${SingleAccount.route}/$accountType")
+                }
+            )
+        }
+        composable(route = Bills.route) {
+            BillsScreen()
+        }
+        composable(
+            route = SingleAccount.routeWithArgs,
+            arguments = SingleAccount.arguments,
+            deepLinks = SingleAccount.deepLinks
+        ) { navBackStackEntry ->
+            val accountType =
+                navBackStackEntry.arguments?.getString(SingleAccount.accountTypeArg)
+
+            SingleAccountScreen(accountType)
+        }
+    }
+}
 
 @Composable
 fun SingleAccountScreen(
